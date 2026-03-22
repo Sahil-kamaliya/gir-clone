924740b63fec628c250e18f25f1c199b4af5ef1b
sahil-kamaliya <kamaliyasahil8@gmail.com>
2026-03-22 10:18:06 +0530

    make the about section fully

--- src/components/Aboutgir.jsx
+++ src/components/Aboutgir.jsx
@@ -2,19 +2,33 @@ import React from 'react'
 
 const Aboutgir = () => {
   return (
-    <div>
-       <div className='bg-[#005865] flex text-white '>
-          <div className=''>
-<h1>About Gir National Park</h1>
-          </div >
-            
-          <div className=''>
-            <p>
-                Located in the west Indian state of Gujarat, Gir National Park or Sasan Gir is an important wildlife reserve for the population of the big cat species of the Asiatic Lion. Inhabited by the endangered species, the forests of Gir are the only place in the world besides Africa where you can spot a lion! Spread over an area of 1412 sq. km. Taking a jungle safari through its forests is the best way to spot one in its natural habitat. Not just home to the Asiatic Lion, Gir Sanctuary also houses a sizable population of leopards, striped hyenas, and jackals among many others.
-Among the 4 national parks and 21 wildlife sanctuaries in Gujarat, Gir Sanctuary is one of the most visited parks by national and international travelers and is a main attraction of Gujarat Tourism. The Asiatic lion population in Gir continues to grow, with 891 lions recorded in the latest count in 2025.
-            </p>
-          </div>
-       </div>
+    <div className='bg-[#005865] py-16 px-6'>
+      <div className='flex flex-col lg:flex-row gap-10 text-white max-w-6xl mx-auto'>
+        
+        <div className='lg:w-1/3'>
+          <h1 className='text-4xl md:text-5xl font-bold'>
+            About Gir National Park
+          </h1>
+            <div className='w-16 h-1 bg-orange-500 mt-4'></div>
+
+        </div>
+          
+        <div className='lg:w-2/3 space-y-5 text-lg'>
+          <p>
+            Located in the west Indian state of Gujarat, Gir National Park or Sasan Gir is an important wildlife 
+            reserve for the population of the big cat species of the Asiatic Lion. Inhabited by the endangered 
+            species, the forests of Gir are the only place in the world besides Africa where you can spot a lion! 
+            Spread over an area of 1412 sq. km. Taking a jungle safari through its forests is the best way to spot 
+            one in its natural habitat. Not just home to the Asiatic Lion, Gir Sanctuary also houses a sizable 
+            population of leopards, striped hyenas, and jackals among many others.
+          </p>
+          <p>
+            Among the 4 national parks and 21 wildlife sanctuaries in Gujarat, Gir Sanctuary is one of the most 
+            visited parks by national and international travelers and is a main attraction of Gujarat Tourism. 
+            The Asiatic lion population in Gir continues to grow, with 891 lions recorded in the latest count in 2025.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
